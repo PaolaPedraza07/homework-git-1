843f3c2e1d5e40499ec7127213cc1f5e311fb172
Paola Pedraza <paolapedraza.3007@gmail.com>
2022-02-09 18:35:40 -0600

    adding sub, div, mult and sqrt methods

--- Operations.java
+++ Operations.java
@@ -2,4 +2,16 @@ public class Operations {
     public int add(int a, int b){
         return a+b;
     }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public double sqrt(double a, double b){
+        return Math.pow(a,b);
+    }
 }
