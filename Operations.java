b328d47ad585e6204c0e14dee597d4fd8bb55669
Paola Pedraza <paolapedraza.3007@gmail.com>
2022-02-10 20:36:17 -0600

    adding message

--- Operations.java
+++ Operations.java
@@ -1,5 +1,18 @@
 public class Operations {
     public int add(int a, int b){
+	System out (“This is add method”);
         return a+b;
+    }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int div(int a, int b){
+        return a/b;
+    }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public double sqrt(double a, double b){
+        return Math.pow(a,b);
     }
 }
