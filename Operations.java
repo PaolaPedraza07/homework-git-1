c7a9f23c24c8dc3ead8c45585246ff3a03e4c3de
Paola Pedraza <paolapedraza.3007@gmail.com>
2022-02-15 19:41:46 -0600

    Using BigInteger instead of int

--- Operations.java
+++ Operations.java
@@ -1,27 +1,29 @@
+import java.math.BigInteger;
+
 public class Operations {
-    public int add(int a, int b){
+    public BigInteger add(BigInteger a, BigInteger b){
         System.out.println("This is add method for 2 params");
         System.out.println("This is add method for 2 params");
-        return a+b;
+        return a.add(b);
     }
-    public int add(int a, int b, int c){
+    public BigInteger add(BigInteger a, BigInteger b, BigInteger c){
         System.out.println("This is add method for 3 params");
-        return a+b+c;
+        return a.add(b).add(c);
     }
-    public int add(int a, int b, int c, int d){
+    public BigInteger add(BigInteger a, BigInteger b, BigInteger c, BigInteger d){
         System.out.println("This is add method for 4 params");
-        return a+b+c+d;
+        return a.add(b).add(c).add(d);
     }
-    public int sub(int a, int b){
-        return a-b;
+    public BigInteger sub(BigInteger a, BigInteger b){
+        return a.subtract(b);
     }
-    public int div(int a, int b){
-        return a/b;
+    public BigInteger div(BigInteger a, BigInteger b){
+        return a.divide(b);
     }
-    public int mult(int a, int b){
-        return a*b;
+    public BigInteger mult(BigInteger a, BigInteger b){
+        return a.multiply(b);
     }
-    public double sqrt(double a, double b){
-        return Math.pow(a,b);
+    public BigInteger sqrt(BigInteger a, int b){
+        return a.pow(b);
     }
 }
