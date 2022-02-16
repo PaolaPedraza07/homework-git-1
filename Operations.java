65231ad9e6169f5d2d05d9f290e5c8fbf78b960b
Paola Pedraza <paolapedraza.3007@gmail.com>
2022-02-15 19:53:19 -0600

    adding intermediate logging for results

--- Operations.java
+++ Operations.java
@@ -4,26 +4,40 @@ public class Operations {
     public BigInteger add(BigInteger a, BigInteger b){
         System.out.println("This is add method for 2 params");
         System.out.println("This is add method for 2 params");
-        return a.add(b);
+        BigInteger result = a.add(b);
+        System.out.println("The result is: "+result);
+        return result;
     }
     public BigInteger add(BigInteger a, BigInteger b, BigInteger c){
         System.out.println("This is add method for 3 params");
-        return a.add(b).add(c);
+        BigInteger result = a.add(b).add(c);
+        System.out.println("The result is: "+result);
+        return result;
     }
     public BigInteger add(BigInteger a, BigInteger b, BigInteger c, BigInteger d){
         System.out.println("This is add method for 4 params");
-        return a.add(b).add(c).add(d);
+        BigInteger result = a.add(b).add(c).add(d);
+        System.out.println("The result is: "+result);
+        return result;
     }
     public BigInteger sub(BigInteger a, BigInteger b){
-        return a.subtract(b);
+        BigInteger result = a.subtract(b);
+        System.out.println("The result is: "+result);
+        return result;
     }
     public BigInteger div(BigInteger a, BigInteger b){
-        return a.divide(b);
+        BigInteger result = a.divide(b);
+        System.out.println("The result is: "+result);
+        return result;
     }
     public BigInteger mult(BigInteger a, BigInteger b){
-        return a.multiply(b);
+        BigInteger result = a.multiply(b);
+        System.out.println("The result is: "+result);
+        return result;
     }
     public BigInteger sqrt(BigInteger a, int b){
-        return a.pow(b);
+        BigInteger result = a.pow(b);
+        System.out.println("The result is: "+result);
+        return result;
     }
 }
